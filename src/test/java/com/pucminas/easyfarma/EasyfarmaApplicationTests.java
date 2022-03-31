948ba5fca827f201750d3574667bba6c6764c48d
Diego Bucher <diego.bucher@partners.mv.com.br>
2022-03-31 18:06:04 -0300

    inicio

---
+++ src/test/java/com/pucminas/easyfarma/EasyfarmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucminas.easyfarma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyfarmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
